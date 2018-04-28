ea77f960b03c98b3c3fa2f238fb3d2dd7cc7bc0a
Caitlin Trussell <caitlintrussell@gmail.com>
2018-04-27 22:08:31 -0500

    fixing bugs with react

--- App.js
+++ App.js
@@ -2,19 +2,15 @@
 import React, { Component } from 'react';
 import {View, Dimensions, StyleSheet} from 'react-native';
 import { ApolloProvider } from 'react-apollo';
-import gql from 'graphql-tag';
+
 import Layout from './ios/app';
-import client from './server/app';
 
 let { height } = Dimensions.get('window');
+
 export default class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
       <Layout />
-
-)
-      </ApolloProvider>
     );
   }
 }
--- ios/app/index.js
+++ ios/app/index.js
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
-import {View, Dimensions, StyleSheet} from 'react-native';
+import {View, Dimensions, StyleSheet, Text} from 'react-native';
 import Login from './components/login/login';
 
 let { height } = Dimensions.get('window');
-
-import { gql } from 'graphql-tag';
-import { Query } from 'react-apollo';
-
-const GET_BURRITOS = gql `
-  {
-    search(term: "burrito", location: "san francisco") {
-      business {
-        name
-      }
-    }
-  }
-  `
-
-const App = () => (
-  <Query query={GET_BURRITOS}>
-    {({ loading, error, data }) => {
-      if (loading) return <div>Loading...</div>;
-      if (error) return <div>Error :(</div>;
-
-      return (
-        <Text>{data.search.businesses[0].name}</Text>
-      )
-    }}
-  </Query>
-)
-
 
 export default class Layout extends Component {
   render() {
     return (
         <View style={styles.container}>
             <View style={[ styles.box1]}></View>
-            <View style={[ styles.box2]}><App /></View>
+            <View style={[ styles.box2]}><Login /></View>
             <View style={[ styles.box3]}></View>
         </View>
     );
